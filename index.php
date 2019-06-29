d1975ed25e0f607147ed14312396666d28fcdb89
enger-estevez <52359038+enger-estevez@users.noreply.github.com>
2019-06-29 14:37:26 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,53 @@
+ <!DOCTYPE html>
+<html>
+	<head>
+		<title>LAKERS</title>
+		<link href="css/Estilo.css" rel="stylesheet" type="text/css">
+	</head>
+	<body background="images/fondo.jpg"  >
+
+		<header id="Cabecera">
+				
+			 
+			<img src="images/Imagen1.png"> 
+			 
+			
+		</header>
+		<nav id="Menu">
+			<ul>
+				<li> <a href="index.html"> INICIO </a>  </li>
+				<li> <a href="nosotros.html"> LAKERS </a>  </li>
+				<li> <a href="index.html"> JUEGOS </a>  </li>
+				<li> <a href="facultades.html"> TICKER </a>  </li>
+				<li> <a href="facultades.html"> VIDEOS </a>  </li>
+				<li> <a href="facultades.html"> CALENDARIO </a>  </li>
+				<li> <a href="facultades.html"> NOTICIAS </a>  </li>
+			</ul>
+		</nav>
+		<section id="Cuerpo">
+				
+                           
+			<section id="Contenido">
+				
+				 <img src="images/Captura.PNG"> 
+							  
+			<center>
+                             <p align="right">
+                             <h3></h3>
+			 </center>
+				</p>
+			</section>
+		</section>
+		<footer id="Pie">
+			<Center>
+			<h3>LAKERS</h3></Center>
+		<Center>	  
+			<h3>Copyright © 2019 NBA Media Ventures, LLC. All rights reserved. 
+						   No portion of NBA.com may be duplicated, redistributed or manipulated in any form. 
+						   By accessing any information beyond this page, you agree to abide by the Privacy Policy 
+						   / Your California Privacy Rights and Terms of Use | Accessibility and Closed Caption | AdChoices</h3>
+                       <h3> .</h3> 
+			</Center>
+		</footer>
+	</body>
+</html>
